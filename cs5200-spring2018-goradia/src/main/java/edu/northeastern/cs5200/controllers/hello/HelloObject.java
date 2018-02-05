15575fb29fd6bc8233a622686dc7fbbab807df28
virat02 <goradia.v@husky.neu.edu>
2018-02-04 20:15:20 -0500

    Integrated Spring Boot With MySQL

--- cs5200-spring2018-goradia/src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ cs5200-spring2018-goradia/src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,23 +1,40 @@
 package edu.northeastern.cs5200.controllers.hello;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name="hello")
 public class HelloObject {
- private String message;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String message;
+
+	 public int getId() {
+		 return id;
+	 }
+
+	 public void setId(int id) {
+		 this.id = id;
+	 }
  
- public String getMessage() {
-  return message;
- }
+	 public String getMessage() {
+		 return message;
+	 }
 
- public void setMessage(String message) {
-  this.message = message;
- }
+	 public void setMessage(String message) {
+		 this.message = message;
+	 }
 
- public HelloObject(String message) {
-  this.message = message;
- }
+	 public HelloObject(String message) {
+		 this.message = message;
+ 	 }
 
- public HelloObject() {
+	 public HelloObject() {
 
- }
+	 }
 
 }
